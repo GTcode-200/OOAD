1199525e7744206c121c782d5c245450d563ef78
Ganesh Tarun S R <ganeshtarunsr101@gmail.com>
2022-01-17 14:32:34 +0530

    17-01-2022

---
+++ Pgm1.java
@@ -0,0 +1,7 @@
+public class Pgm1
+{
+    public static void main(String args[]) //static->function can be called without creating object
+    {                                      //main is called by jvm outside the class
+        System.out.println("Hello world"); //out-object of system class.  println is function of out class
+    }
+}
